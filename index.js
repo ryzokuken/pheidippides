266137c60ef1bc14fe4bf0c5c625fdc7533076b9
Ujjwal Sharma <usharma1998@gmail.com>
2018-10-17 16:32:43 +0530

    src: yay

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-const username = process.argv[2];
-const password = process.argv[3];
+const root = 'https://facebook.com';
+
+const [username, password, groupsPath, imagePath, contentPath] = process.argv.slice(2);
+const groups = fs
+  .readFileSync(groupsPath)
+  .toString('utf-8')
+  .trim()
+  .split('\n');
+const content = fs
+  .readFileSync(contentPath)
+  .toString('utf-8')
+  .trim();
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false, args: ['--disable-notifications'] });
   const page = await browser.newPage();
-  await page.goto('http://facebook.com/', { waitUntil: 'networkidle0' });
 
+  // LOGIN
+  await page.goto(root, { waitUntil: 'domcontentloaded' });
   await page.type('#email', username);
   await page.type('#pass', password);
-  await page.click('#u_0_2');
-  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  await page.click('#loginbutton');
+  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
+  // GROUPS
+  for (let i = 0; i < groups.length; i++) {
+    await page.goto(`${root}/groups/${groups[i]}/`, { waitUntil: 'load' });
+    await page.type('textarea[name="xhpc_message_text"]', content);
+    const input = await page.$('input[name="composer_photo[]"]');
+    await input.uploadFile(imagePath);
+    await page.waitFor(5000);
+    let disabled = true;
+    while (disabled)
+      disabled = await page.$eval(
+        'button[data-testid=react-composer-post-button]',
+        button => button.disabled
+      );
+    await page.click('button[data-testid=react-composer-post-button]');
+    await page.waitFor(5000);
+  }
+
+  // CLEANUP
   await browser.close();
 })();
